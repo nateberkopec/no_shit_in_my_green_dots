e2cc91c1b1a8844b5509db3e5cbfe51c25ecdd94
Nate Berkopec <nate.berkopec@gmail.com>
2025-11-20 12:25:50 +0900

    bundle gem

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in no_shit_in_my_green_dots.gemspec
+gemspec
+
+gem "irb"
+gem "rake", "~> 13.0"
---
+++ lib/no_shit_in_my_green_dots.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "no_shit_in_my_green_dots/version"
+
+module NoShitInMyGreenDots
+  class Error < StandardError; end
+  # Your code goes here...
+end
